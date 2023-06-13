3be436b2bbd54a162545b10f16a2e8cbe5241511
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-13 16:28:39 +0200

    fix style card

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,22 @@
       {{ title }}
     </h1>
     <div class="row">
-      <div class="col" v-for="(project, index) in projects" :key="project.id">
-        {{ project.title }}
+      <div
+        class="col-12 col-sm-6 col-md-4 col-lg-3 p-3 mt-2"
+        v-for="(project, index) in projects"
+        :key="project.id"
+      >
+        <div class="card">
+          <div class="card-img-top">
+            <img class="img-fluid" :src="project.image" alt="" srcset="" />
+          </div>
+          <div class="card-title">
+            <h2 class="fs-1">{{ project.title }}</h2>
+          </div>
+          <div class="card-body">
+            {{ project.body }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -35,4 +49,6 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use "./assets/style/general.scss" as *;
+</style>
