32a15ecfcb12318c75a4141b21f5c1df5eedad28
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-13 15:59:51 +0200

    linked with api and show in web

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
-<script setup></script>
-
 <template>
-  <h1>SONO VIVO</h1>
+  <div class="container">
+    <h1>
+      {{ title }}
+    </h1>
+    <div class="row">
+      <div class="col" v-for="(project, index) in projects" :key="project.id">
+        {{ project.title }}
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Le mie API",
+      projects: [],
+      apiUrl: "http://127.0.0.1:8001/api",
+    };
+  },
+  methods: {
+    getData() {
+      axios.get(`${this.apiUrl}/projects`).then((res) => {
+        this.projects = res.data.results.data;
+      });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
+</script>
 
 <style scoped></style>
