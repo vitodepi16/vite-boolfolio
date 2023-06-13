a27e80753896a425d0f6fdbfd68a0317dc67d1bf
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-13 16:39:30 +0200

    fix style

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,35 @@
     </h1>
     <div class="row">
       <div
-        class="col-12 col-sm-6 col-md-4 col-lg-3 p-3 mt-2"
+        class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex p-3 mt-4"
         v-for="(project, index) in projects"
         :key="project.id"
       >
-        <div class="card">
-          <div class="card-img-top">
-            <img class="img-fluid" :src="project.image" alt="" srcset="" />
-          </div>
-          <div class="card-title">
-            <h2 class="fs-1">{{ project.title }}</h2>
-          </div>
+        <div class="card" style="width: 18rem">
+          <img class="img-fluid" :src="project.image" alt="" srcset="" />
           <div class="card-body">
-            {{ project.body }}
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.body }}</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              Progetto creato usando: <strong> {{ project.type.name }}</strong>
+            </li>
+            <li class="list-group-item">Creato il: {{ project.created_at }}</li>
+          </ul>
+          <div class="card-body d-flex">
+            <form action="{{ project.slug }}" method="POST">
+              <a class="btn-outline-primary btn mx-auto" href=""
+                ><i class="fa-solid fa-eye"></i
+              ></a>
+              <button
+                type="submit"
+                class="mx-auto delete-button btn btn-outline-danger"
+                data-item-title="{{ $project.title }}"
+              >
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </form>
           </div>
         </div>
       </div>
